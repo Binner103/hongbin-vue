c1073899492a8393a4f3a5f410ddf6dda5e19191
nhb1998 <849990475@qq.com>
2022-02-13 18:01:12 +0800

    配置

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    loading: false,
+    messages: [] as Array<string>
   },
+
   mutations: {
+    setLoading(state, data) {
+      state.loading = data;
+    },
+
+    setMessages(state, data) {
+      state.messages = [...state.messages, ...data];
+    }
   },
+
   actions: {
+    getMessages({ commit }) {
+      commit('setLoading', true);
+      setTimeout(() => {
+        commit('setLoading', false);
+        commit('setMessages', ['您好!', 'hello~', 'hola !']);
+      }, 1000);
+    },
   },
   modules: {
   }
