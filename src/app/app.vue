f42ba3cacd33db08bf68a9f77d9003ddb16a6eb3
nhb1998 <849990475@qq.com>
2022-02-13 22:06:42 +0800

    文本框(Text)

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <h3>{{ name }}</h3>
-  
+  <input type="text" v-model="content">
+  <span>→ Content: {{ content }}</span>
 </template>
 
 <script>
@@ -8,6 +9,7 @@ export default {
   data() {
     return {
       name: 'HONGBIN',
+      content: '天地玄黄'
     };
   },
 };
