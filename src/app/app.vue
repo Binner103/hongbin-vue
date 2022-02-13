0406938b09f3b40b5373d4703f9e9573264a5074
nhb1998 <849990475@qq.com>
2022-02-13 21:37:37 +0800

    清理

---
+++ src/app/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>{{ name }}</h3>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'HONGBIN',
+    };
+  },
+};
+</script>
+
+<style>
+@import './styles/app.css';
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './app/app.vue';
 
-createApp(App).use(store).use(router).mount('#app')
+/**
+ * 创建应用
+ */
+const app = createApp(App);
+
+/**
+ * 挂载应用
+ */
+app.mount('#app');
