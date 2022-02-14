30cebe06bb12eed9fd2d6698380281958dd9463a
nhb1998 <849990475@qq.com>
2022-02-14 15:50:11 +0800

    单选按钮(radio)

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,18 +1,24 @@
 <template>
   <h3>{{ name }}</h3>
-  <label for="tag-1">
-    <input id="tag-1" type="checkbox" value="大明湖" v-model="tags">
-    大明湖
+  <label for="subscription-annual">
+    <input
+      type="radio"
+      value="annual"
+      id="subscription-annual"
+      v-model="subscription"
+    />
+    一年
   </label>
-  <label for="tag-2">
-    <input id="tag-2" type="checkbox" value="日月潭" v-model="tags">
-    日月潭
+  <label for="subscription-quarterly">
+    <input
+      type="radio"
+      value="quarterly"
+      id="subscription-quarterly"
+      v-model="subscription"
+    />
+    季度
   </label>
-  <label for="tag-3">
-    <input id="tag-3" type="checkbox" value="鄱阳湖" v-model="tags">
-    鄱阳湖
-  </label>
-  <span>→ Tags: {{ tags }}</span>
+  <span>→subscription: {{subscription}}</span>
 </template>
 
 <script>
@@ -20,7 +26,7 @@ export default {
   data() {
     return {
       name: 'HONGBIN',
-      tags:[]
+      subscription: 'annual',
     };
   },
 };
