c8aa8b35c3f1f2cba22ced7e78dd7a20b134ad67
nhb1998 <849990475@qq.com>
2022-02-15 00:45:07 +0800

    props: Vue组件的属性

--- src/app/components/app-button.vue
+++ src/app/components/app-button.vue
@@ -1,20 +1,31 @@
 <template>
-    <button class="button">按钮</button>
+  <button class="button">{{text}}</button>
 </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '按钮',
+    },
+  },
+};
+</script>
 
 <style scoped>
 .button {
-    outline: none;
-    border: none;
-    background: #e1e1e1;
-    color: #5a5a5a;
-    padding: 8px 16px;
-    margin: 4px;
-    cursor: pointer;
-    border-radius: 3px;
+  outline: none;
+  border: none;
+  background: #e1e1e1;
+  color: #5a5a5a;
+  padding: 8px 16px;
+  margin: 4px;
+  cursor: pointer;
+  border-radius: 3px;
 }
 
 .button:active {
-    opacity: 0.8;
+  opacity: 0.8;
 }
 </style>
