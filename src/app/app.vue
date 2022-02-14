887634f9577114bc23e0e663d183a0b0922d7d59
nhb1998 <849990475@qq.com>
2022-02-14 23:53:19 +0800

    定义与使用Vue组件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,22 @@
 <template>
   <h3>{{ name }}</h3>
-  
+  <AppButton></AppButton>
+  <AppButton />
+  <app-button></app-button>
+  <app-button />
 </template>
 
 <script>
+import AppButton from './components/app-button.vue';
+
 export default {
   data() {
     return {
       name: 'HONGBIN',
     };
   },
+
+  components: { AppButton }
 };
 </script>
 
