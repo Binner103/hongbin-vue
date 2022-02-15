dc37037cefd1ca5c0d1e4ca34bf4a58f4bb76e23
nhb1998 <849990475@qq.com>
2022-02-15 20:21:36 +0800

    在自定义组件上使用v-model

---
+++ src/app/components/app-input.vue
@@ -0,0 +1,11 @@
+<template>
+    <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: String
+    },
+}
+</script>
