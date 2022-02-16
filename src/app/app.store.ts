3884abdc7b6deaa63b0ff161cc14e7a040881c83
nhb1998 <849990475@qq.com>
2022-02-16 21:29:56 +0800

    actions 动作

---
+++ src/app/app.store.ts
@@ -0,0 +1,31 @@
+import { createStore } from "vuex";
+
+/**
+ * 创建store
+ */
+const store = createStore({
+  state: {
+    name: ''
+  },
+
+  getters: {
+    name(state) {
+      return `🎈${state.name}`
+    }
+  },
+
+  mutations: {
+    setName(state, data) {
+      state.name = data;
+    }
+  },
+
+  actions: {
+    getName(context) {
+      const name = '璇彬网';
+      context.commit('setName', name)
+    }
+  }
+});
+
+export default store;
--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,35 @@
 <template>
-  <h3>{{ name }}</h3>
-  
+  <h3 @click="onClickName">{{ name }}</h3>
 </template>
 
 <script>
+import { mapState, mapGetters, mapMutations} from 'vuex';
+
 export default {
   data() {
-    return {
-      name: 'HONGBIN',
-    };
+    return {};
   },
+
+  computed: {
+    ...mapGetters(['name'])
+  },
+
+  created() {
+    this.$store.dispatch('getName')
+  },
+
+  methods: {
+    ...mapMutations(['setName']),
+    onClickName() {
+      if (this.$store.state.name === '璇彬网') {
+        // this.$store.commit('setName', 'XUANBIN')
+        this.setName('XUANBIN')
+      } else {
+        // this.$store.commit('setName', '璇彬网')
+        this.setName('璇彬网')
+      }
+    }
+  }
 };
 </script>
 
