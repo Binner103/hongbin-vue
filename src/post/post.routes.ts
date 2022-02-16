c18cfba228d6d031efcd44b9c4831eff70426098
nhb1998 <849990475@qq.com>
2022-02-16 18:58:35 +0800

    动态路由

--- src/post/post.routes.ts
+++ src/post/post.routes.ts
@@ -1,5 +1,6 @@
 import { RouteRecordRaw } from 'vue-router';
 import PostIndex from './index/post-index.vue';
+import PostShow from './show/post-show.vue';
 
 /**
  * 定义路由
@@ -8,6 +9,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/posts',
     component: PostIndex,
+  },
+  {
+    path: '/posts/:postId',
+    component: PostShow
   },
 ];
 
