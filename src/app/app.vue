43991373b6ffcd0e1df3412621374e1059e600cd
nhb1998 <849990475@qq.com>
2022-02-17 23:34:38 +0800

    声明具有反应特性的数据 reactive

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,18 +1,26 @@
 <template>
   <h3 @click="name = 'YUXUAN'">{{ name }}</h3>
+  <h4 @click="user.name = '玉旋'">{{user.name}}</h4>
 </template>
 
 <script>
-import {ref} from 'vue';
+import {reactive, ref} from 'vue';
 
 export default {
   setup() {
     const name = ref('HONGBIN')
 
+    const user = reactive({
+      name: '宏彬'
+    });
+
+    console.log(user);
+
     console.log(name.value);
 
     return {
       name,
+      user
     }
   }
 };
