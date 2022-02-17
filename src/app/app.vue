b5b97fd61a35d4532a16cbde5e789b4c1abac961
nhb1998 <849990475@qq.com>
2022-02-17 23:18:26 +0800

    JavaScript: Proxy与Reflect

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <h3>{{ name }}</h3>
-  
 </template>
 
 <script>
@@ -9,6 +8,38 @@ export default {
     return {
       name: 'HONGBIN',
     };
+  },
+
+  created() {
+    const user = {
+      name: 'HONGBIN',
+    };
+
+    const handler = {
+      get(target, property) {
+        return target[property];
+      },
+
+      set(target, property, value) {
+        if (property === 'name') {
+          if (value.length > 10) {
+            throw Error('名字太长了');
+          }
+        }
+
+        return Reflect.set(target, property, value);
+      },
+    };
+
+    const userProxy = new Proxy(user, handler);
+
+    try {
+      userProxy.name = 'HONGBINHONGBINHONGBIN'
+    } catch (error) {
+      console.log('错误:', error.message);
+    }
+
+    console.log(userProxy.name);
   },
 };
 </script>
