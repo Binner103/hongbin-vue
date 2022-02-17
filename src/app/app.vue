823cfca84f1fbb6a280de0d9a4a8dc9d31331706
nhb1998 <849990475@qq.com>
2022-02-17 20:42:38 +0800

    transition name: 指定过渡效果的名字

--- src/app/app.vue
+++ src/app/app.vue
@@ -6,7 +6,7 @@
         <div class="card-subtitle">Transition & Animation</div>
       </div>
       <div class="card_content">
-        <transition>
+        <transition name="slide">
         <div v-if="isActive" class="emoji">😍</div>
         </transition>
       </div>
