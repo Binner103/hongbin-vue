3f9c77afefd70e663f563fd19bbf63a0d817d336
nhb1998 <849990475@qq.com>
2022-02-17 23:23:06 +0800

    理解Vue的反应系统

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3>{{ name }}</h3>
+  <h3 @click="name = 'YUXUAN'">{{ name }}</h3>
 </template>
 
 <script>
@@ -11,36 +11,8 @@ export default {
   },
 
   created() {
-    const user = {
-      name: 'HONGBIN',
-    };
-
-    const handler = {
-      get(target, property) {
-        return target[property];
-      },
-
-      set(target, property, value) {
-        if (property === 'name') {
-          if (value.length > 10) {
-            throw Error('名字太长了');
-          }
-        }
-
-        return Reflect.set(target, property, value);
-      },
-    };
-
-    const userProxy = new Proxy(user, handler);
-
-    try {
-      userProxy.name = 'HONGBINHONGBINHONGBIN'
-    } catch (error) {
-      console.log('错误:', error.message);
-    }
-
-    console.log(userProxy.name);
-  },
+    console.log(this.$data);
+  }
 };
 </script>
 
