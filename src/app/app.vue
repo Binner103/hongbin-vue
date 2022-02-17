42c68bdfb94567c5f049dedccd5a6b8bdb94cbd1
nhb1998 <849990475@qq.com>
2022-02-17 21:54:00 +0800

    组件之间的过渡

--- src/app/app.vue
+++ src/app/app.vue
@@ -10,8 +10,9 @@
           name="custom-classes"
           enter-active-class="animate__animated animate__tada"
           leave-active-class="animate__animated animate__bounce"
+          mode="out-in"
         >
-          <div v-if="isActive" class="emoji">😍</div>
+          <component :is="currentEmoji"></component>
         </transition>
       </div>
       <div class="card_action">
@@ -27,13 +28,28 @@
 </template>
 
 <script>
+import GhostEmoji from './components/ghost-emoji.vue'
+import RobotEmoji from './components/robot-emoji.vue'
+
 export default {
   data() {
     return {
       name: 'HONGBIN',
       isActive: true,
+      emoji: 'GhostEmoji'
     };
   },
+
+  computed: {
+    currentEmoji() {
+      return this.isActive ? 'GhostEmoji' : 'RobotEmoji'
+    }
+  },
+
+  components: {
+    GhostEmoji,
+    RobotEmoji
+  }
 };
 </script>
 
