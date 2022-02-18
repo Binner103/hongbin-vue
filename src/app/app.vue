3b9a5dd692fb2aca47b74ab731ea255d564a9d1c
nhb1998 <849990475@qq.com>
2022-02-18 19:04:48 +0800

    在请求里配置头部数据

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
+  <input type="text" v-model="title" @keyup.enter="createPost" />
   <div>{{ errorMessage }}</div>
   <div v-for="post in posts" :key="post.id">
     {{ post.title }} -
@@ -21,17 +22,12 @@ export default {
         password: '123123',
       },
       token: '',
+      title: '',
     };
   },
 
   async created() {
-    try {
-      const response = await apiHttpClient.get('/posts');
-
-      this.posts = response.data;
-    } catch (error) {
-      this.errorMessage = error.message;
-    }
+    this.getPosts();
 
     //用户登录
     try {
@@ -41,6 +37,41 @@ export default {
     } catch (error) {
       this.errorMessage = error.message;
     }
+  },
+
+  methods: {
+    async getPosts() {
+      try {
+        const response = await apiHttpClient.get('/posts');
+
+        this.posts = response.data;
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+
+    async createPost() {
+      try {
+        const response = await apiHttpClient.post(
+          '/posts',
+          {
+            title: this.title,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${this.token}`,
+            },
+          },
+        );
+
+        console.log(response.data);
+
+        this.title = '';
+        this.getPosts();
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
   },
 };
 </script>
