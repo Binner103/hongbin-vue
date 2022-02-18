ddd31d35fe981549f4296af1c37afb0651234e95
nhb1998 <849990475@qq.com>
2022-02-19 00:09:25 +0800

    退出登录

--- src/app/app.vue
+++ src/app/app.vue
@@ -9,6 +9,7 @@
 
   <div v-if="currentUser">
     <div>{{ currentUser.name }}</div>
+    <button @click="logout">退出</button>
   </div>
 
   <input
@@ -69,6 +70,14 @@ export default {
   },
 
   methods: {
+    logout() {
+      this.token = '';
+      this.currentUser = null;
+
+      localStorage.removeItem('tid');
+      localStorage.removeItem('uid');
+    },
+
     async getCurrentUser(userId) {
       try {
         const response = await apiHttpClient.get(`/users/${userId}`);
