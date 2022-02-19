0b76ff4b3215b34dce8d61f560eed795f7e74f55
nhb1998 <849990475@qq.com>
2022-02-19 16:45:14 +0800

    应用的数据管理（Vuex）：创建与使用 Store

---
+++ src/app/app.store.ts
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex';
+
+/**
+ * 创建store
+ */
+const store = createStore({
+  state: {
+    appName: 'HONGBIN'
+  }
+});
+
+/**
+ * 导出store
+ */
+export default store;
